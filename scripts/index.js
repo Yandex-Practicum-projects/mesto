27d26a9cd2d569cc5122a4215af733d92a175354
bababum <bababum1995@gmail.com>
2023-02-12 21:12:54 +0700

    validate form

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,13 @@
-const initialCards = [
-    {
-        name: 'Дананг',
-        link: 'https://images.unsplash.com/photo-1564596823821-79b97151055e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80'
-    },
-    {
-        name: 'Кабул',
-        link: 'https://images.unsplash.com/photo-1673860503924-e8085c807ca1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-        name: 'Токио',
-        link: 'https://images.unsplash.com/photo-1605098096512-f980b1ac818d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80'
-    },
-    {
-        name: 'Лиссабон',
-        link: 'https://images.unsplash.com/photo-1578859651203-c7126a106b59?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80'
-    },
-    {
-        name: 'Гонконг',
-        link: 'https://images.unsplash.com/photo-1506970845246-18f21d533b20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
 const gridZona = document.querySelector('.grid-zona')
 const cardTemplateContainer = document
-.querySelector('.template')
-.content.querySelector('.card');
+    .querySelector('.template')
+    .content.querySelector('.card');
 const btnAddNewPlace = document.querySelector('.profile__add')
 const btnEditProfile = document.querySelector('.profile__edit')
 const fullName = document.querySelector('.profile__name')
 const about = document.querySelector('.profile__about')
 const newPlace = document.querySelector('.popup_new-place')
+const newPlaceForm = newPlace.querySelector('.popup__form')
 const btnCloseNewPlace = newPlace.querySelector('.popup__close')
 const nameNewPlace = newPlace.querySelector('.popup__name')
 const linkNewPlace = newPlace.querySelector('.popup__link')
@@ -46,14 +20,32 @@ const fullscreenImage = fullscreenCard.querySelector('.popup__card-image')
 const fullscreenTitle = fullscreenCard.querySelector('.popup__card-title')
 const btnFullscreenClose = fullscreenCard.querySelector('.popup__close')
 
+popupName.value = fullName.textContent;
+popupAbout.value = about.textContent;
+
 renderCards(initialCards)
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened')
+    document.removeEventListener('keydown', keyHandler)
+    popup.removeEventListener('click', keyHandler)
 }
 function openPopup(popup) {
     popup.classList.add('popup_opened')
+    document.addEventListener('keydown', keyHandler)
+    popup.addEventListener('click', keyHandler)
 }
+
+function keyHandler(evt) {
+    const popupOpened = document.querySelector('.popup_opened')
+    if (evt.key === 'Escape') {
+        closePopup(popupOpened)
+    }
+    if (evt.target === popupOpened) {
+        closePopup(popupOpened)
+    }
+}
+
 function cardDelete(btn) {
     btn.target.closest('.card').remove();
 }
@@ -61,7 +53,7 @@ function openCard(name, link) {
     fullscreenTitle.textContent = name
     fullscreenImage.src = link
     fullscreenImage.alt = name
-    openPopup(fullscreenCard) 
+    openPopup(fullscreenCard)
 }
 function toggleLike(btn) {
     btn.target.classList.toggle('card__like_active')
@@ -87,22 +79,21 @@ function addCards(name, link) {
 }
 
 function renderCards(elements) {
-const cards = elements.map((element) => {
-    const name = element.name
-    const link = element.link
-    return addCards(name, link)
-})
-gridZona.append(...cards)
+    const cards = elements.map((element) => {
+        const name = element.name
+        const link = element.link
+        return addCards(name, link)
+    })
+    gridZona.append(...cards)
 }
 
-newPlace.addEventListener('submit', (evt) => {
+newPlaceForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const name = nameNewPlace.value;
     const link = linkNewPlace.value;
+    newPlaceForm.reset();
     gridZona.prepend(addCards(name, link))
-    closePopup(newPlace)
-    nameNewPlace.value = ''
-    linkNewPlace.value = ''
+    closePopup(newPlace);
 })
 
 editProfile.addEventListener('submit', (evt) => {
@@ -126,7 +117,5 @@ btnAddNewPlace.addEventListener('click', () => {
     openPopup(newPlace)
 })
 btnEditProfile.addEventListener('click', () => {
-    popupName.value = fullName.textContent;
-    popupAbout.value = about.textContent;
     openPopup(editProfile)
 })
